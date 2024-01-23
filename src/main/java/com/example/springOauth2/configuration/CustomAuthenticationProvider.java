7c9e0e5848931cddbee3e704c872b29c77dacb50
Ridho Pratama Putra <ridho.putra@btpn.com>
2024-01-23 22:13:37 +0700

    try to return fake jwt

--- src/main/java/com/example/springOauth2/configuration/CustomAuthenticationProvider.java
+++ src/main/java/com/example/springOauth2/configuration/CustomAuthenticationProvider.java
@@ -1,11 +1,13 @@
 package com.example.springOauth2.configuration;
 
+import java.time.Instant;
+
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,8 +18,8 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
         Object principal = authentication.getPrincipal();
         String principalWithLdapUserNameSuffix = authentication.getPrincipal().toString();
         String credential = authentication.getCredentials().toString();
-
-        throw new AuthenticationServiceException("e.getMessage()");
+        Jwt jwt = new Jwt("credential", Instant.now(), Instant.now(), null, null);
+        return new JwtAuthenticationToken(jwt, null);
     }
 
     @Override
